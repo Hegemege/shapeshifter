98a3387aee7705c1a82eb0122081374b428d0db0
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2016-11-01 13:58:07 +0200

    added player controller continued

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour
+{
+	public Shape StartingShape;
+	public List<Shape> Shapes;
+
+	[HideInInspector]
+	public Shape CurrentShape;
+
+	void Start()
+	{
+		ShiftTo(StartingShape);
+	}
+
+	void Update()
+	{
+		if (Input.GetKeyDown("space"))
+		{
+			ShiftTo(NextShape());
+		}
+	}
+
+	Shape NextShape()
+	{
+		int index = Shapes.IndexOf(CurrentShape) + 1;
+		if (index >= Shapes.Count) index = 0;
+		return Shapes[index];
+	}
+
+	void ShiftTo(Shape toShape)
+	{
+		foreach (Shape shape in Shapes)
+		{
+			if (shape.Equals(toShape))
+			{
+				CurrentShape = shape;
+				shape.gameObject.SetActive(true);
+			}
+			else
+			{
+				shape.gameObject.SetActive(false);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shape : MonoBehaviour
+{
+	[HideInInspector]
+	Rigidbody rb;
+
+	CommonInputManager inputManager;
+
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Start()
+	{
+		inputManager = CommonInputManager.instance;
+	}
+
+	void Update()
+	{
+		Debug.Log(inputManager.HorizontalInput);
+		float dt = Time.deltaTime;
+		rb.AddTorque(
+			inputManager.HorizontalInput * dt * 100,
+			0f,
+			inputManager.VerticalInput * dt * 100
+		);
+	}
+}
